9538ccdbd518730e000a57a4f4ca192fa6544b66
qzkinhit <58599846+qzkinhit@users.noreply.github.com>
2024-07-02 15:35:50 +0800

    Update testcleantable.py

--- testcleantable.py
+++ testcleantable.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 from pyspark.sql import SparkSession
-import logging
 
 def main():
-    # 配置日志级别
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    
-    # 创建SparkSession并配置特定参数
+    # 创建SparkSession并配置以访问Spark元数据
     spark = SparkSession.builder \
         .appName("DataCleaning") \
         .config("spark.sql.session.state.builder", "org.apache.spark.sql.hive.UQueryHiveACLSessionStateBuilder") \
@@ -16,19 +11,16 @@ def main():
         .enableHiveSupport() \
         .getOrCreate()
 
-    try:
-        # 读取数据湖中的表格信息
-        query = "SELECT * FROM tid_sdi_ai4data.ai4data_enterprise_bak LIMIT 100"  # 仅读取前100行进行示例
-        df = spark.sql(query)
-        
-        # 显示原始数据
-        logger.info("Original Data:")
-        df.show()
-    except Exception as e:
-        logger.error("Error occurred while executing the query or displaying the data.", exc_info=True)
-    finally:
-        # 停止SparkSession
-        spark.stop()
+    # 读取数据湖中的表格信息
+    query = "SELECT * FROM tid_sdi_ai4data.ai4data_enterprise_bak LIMIT 100"  # 仅读取前100行进行示例
+    df = spark.sql(query)
+
+    # 显示原始数据
+    print("Original Data:")
+    df.show()
+
+    # 停止SparkSession
+    spark.stop()
 
 if __name__ == "__main__":
     main()
